f7ae5f7394c380f755007cdcd56c663f43231a80
Ariana Horn <74698341+Arianahorn@users.noreply.github.com>
2022-07-01 18:06:32 -0700

    Add files via upload

---
+++ ANA515 Final Assignment.R
@@ -0,0 +1,15 @@
+#Assignment 2 - getting and cleaning data
+#checking wd 
+getwd()
+
+#downloading bulk storm details data for 1994
+#loading data into R 
+#loading packages I will use 
+install.packages("tidyverse")
+install.packages("dplyr")
+library("tidyverse")
+library("dplyr")
+#2. Now I am supposed to limit my dataframe to just include certain variables. 
+#select variables 
+
+
